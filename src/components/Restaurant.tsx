c42597bc9d146e35cca9646e782eed08ae23e9b4
Chris Wade <chrisw@brightervision.com>
2020-10-16 17:03:37 -0600

    Addition ts stuff; added minireset and initial component

---
+++ src/components/Restaurant.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Restaurant: React.FC = () => {
+  return (
+    <div className="restaurant">
+      This is my restaurant
+    </div>
+  )
+}
+
+export default Restaurant;
